69a230c35ff6e9214290913fddf27e0618c6ff11
jkresner <jkresner@yahoo.com.au>
2013-09-20 11:43:21 -0700

    How bout now?

--- wp-content/themes/twentytwelve/page-tech.php
+++ wp-content/themes/twentytwelve/page-tech.php
@@ -1,18 +1,10 @@
-<?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
- */
+< ?php
+/*
+Template Name: About
+*/
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
 
   <link rel="stylesheet" href="http://www.airpair.com/stylesheets/landing_jk.css">
 
