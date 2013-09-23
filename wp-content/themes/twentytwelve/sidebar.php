e2d175f11eaa274527a2bfcdc93a05a2731aad26
jkresner <jkresner@yahoo.com.au>
2013-09-23 10:20:49 -0700

    Couple of small social follow fixes

--- wp-content/themes/twentytwelve/sidebar.php
+++ wp-content/themes/twentytwelve/sidebar.php
@@ -15,15 +15,16 @@
 		<div id="secondary" class="widget-area" role="complementary">
 
       <style type="text/css">
-        .social { height:40px; }
-        .social li { float:left;margin-right:5px; }
+        .social { height:80px; }
+        .social li { float:left;margin-right:6px; }
       </style>
 
+      <label>Follow AirPair</label>
       <ul class="social">
-        <li><a href="http://twitter.com/airpair"><img src="/img/tw.png" /></a></li>
-        <li><a href="http://facebook.com/airpair"><img src="/img/fb.png" /></a></li>
-        <li><a href="https://plus.google.com/105075583437166504731‎"><img src="/img/gp.png" /></a></li>
-        <li><a href="http://www.linkedin.com/company/airpair"><img src="/img/in.png" /></a></li>
+        <li><a href="http://twitter.com/airpair" target="_blank"><img src="/img/tw.png" /></a></li>
+        <li><a href="http://facebook.com/airpair" target="_blank"><img src="/img/fb.png" /></a></li>
+        <li><a href="https://plus.google.com/105075583437166504731" target="_blank"><img src="/img/gp.png" /></a></li>
+        <li><a href="http://www.linkedin.com/company/airpair" target="_blank"><img src="/img/in.png" /></a></li>
       </ul>
 
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
