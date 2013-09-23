0fa53452bca18670640f22fb041992f6079175c9
jkresner <jkresner@yahoo.com.au>
2013-09-23 10:10:04 -0700

    Trying social links

--- wp-content/themes/twentytwelve/sidebar.php
+++ wp-content/themes/twentytwelve/sidebar.php
@@ -11,6 +11,14 @@
 ?>
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+
+    <ul class="social">
+      <li><a href="http://twitter.com/airpair"><img src="/img/tw.png" /></a></li>
+      <li><a href="http://facebook.com/airpair"><img src="/img/fb.png" /></a></li>
+      <li><a href="https://plus.google.com/105075583437166504731‎"><img src="/img/gp.png" /></a></li>
+      <li><a href="http://www.linkedin.com/company/airpair"><img src="/img/in.png" /></a></li>
+    </ul>
+
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</div><!-- #secondary -->
