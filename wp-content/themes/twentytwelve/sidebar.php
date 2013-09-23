48827929e8a314a90af07b389fbed182e2caa7c0
jkresner <jkresner@yahoo.com.au>
2013-09-23 13:16:05 -0700

    Improving sidebar ui

--- wp-content/themes/twentytwelve/sidebar.php
+++ wp-content/themes/twentytwelve/sidebar.php
@@ -14,23 +14,27 @@
 
 		<div id="secondary" class="widget-area" role="complementary">
 
-      <h3 class="widget-title">Need expert help?</h3>
+      <div class="side-wid">
+        <h3 class="widget-title">Need expert help?</h3>
+        <br />
+        <a href="http://airpa.ir/ap-blog-side-find" target="_blank"><img src="/img/find.png" alt="find an expert" /></a>
+      </div>
 
-      <a href="http://airpa.ir/ap-blog-side-find"><img src="/img/find.png" alt="find an expert" /></a>
+      <div class="side-wid">
+        <h3 class="widget-title">Follow AirPair for updates</h3>
+        <ul class="social">
+          <li><a href="http://twitter.com/airpair" target="_blank"><img src="/img/tw.png" /></a></li>
+          <li><a href="http://facebook.com/airpair" target="_blank"><img src="/img/fb.png" /></a></li>
+          <li><a href="https://plus.google.com/105075583437166504731" target="_blank"><img src="/img/gp.png" /></a></li>
+          <li><a href="http://www.linkedin.com/company/airpair" target="_blank"><img src="/img/in.png" /></a></li>
+          <li><a href="http://angel.co/airpair" target="_blank"><img src="/img/al.png" /></a></li>
+        </ul>
+      </div>
 
-      <h3 class="widget-title">Follow AirPair for updates</h3>
-
-      <ul class="social">
-        <li><a href="http://twitter.com/airpair" target="_blank"><img src="/img/tw.png" /></a></li>
-        <li><a href="http://facebook.com/airpair" target="_blank"><img src="/img/fb.png" /></a></li>
-        <li><a href="https://plus.google.com/105075583437166504731" target="_blank"><img src="/img/gp.png" /></a></li>
-        <li><a href="http://www.linkedin.com/company/airpair" target="_blank"><img src="/img/in.png" /></a></li>
-        <li><a href="http://angel.co/airpair" target="_blank"><img src="/img/al.png" /></a></li>
-      </ul>
-
-      <h3 class="widget-title">Subscribe by Email</h3>
-
-      <form action='https://www.getvero.com/forms/33b965c1318bd3ff2ec43d39839c2202' method='post'><br/>    <label>Your email</label><input name='email' type='email'></input><br/>    <input type='submit' value='Submit form' /><br/></form>
+      <div class="side-wid">
+        <h3 class="widget-title">Subscribe by Email</h3>
+        <form action='https://www.getvero.com/forms/33b965c1318bd3ff2ec43d39839c2202' method='post'><br/>    <label>Your email</label><input name='email' type='email'></input><br/>    <input type='submit' value='Submit form' /><br/></form>
+      </div>
 
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</div><!-- #secondary -->
