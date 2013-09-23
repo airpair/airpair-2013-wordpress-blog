7b15106663eee2e8e35a8626429b696119298d8d
jkresner <jkresner@yahoo.com.au>
2013-09-23 10:16:44 -0700

    Fix social css

--- wp-content/themes/twentytwelve/sidebar.php
+++ wp-content/themes/twentytwelve/sidebar.php
@@ -12,14 +12,20 @@
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
-    <ul class="social">
-      <li><a href="http://twitter.com/airpair"><img src="/img/tw.png" /></a></li>
-      <li><a href="http://facebook.com/airpair"><img src="/img/fb.png" /></a></li>
-      <li><a href="https://plus.google.com/105075583437166504731‎"><img src="/img/gp.png" /></a></li>
-      <li><a href="http://www.linkedin.com/company/airpair"><img src="/img/in.png" /></a></li>
-    </ul>
-
 		<div id="secondary" class="widget-area" role="complementary">
+
+      <style type="text/css">
+        .social { height:40px; }
+        .social li { float:left;margin-right:5px; }
+      </style>
+
+      <ul class="social">
+        <li><a href="http://twitter.com/airpair"><img src="/img/tw.png" /></a></li>
+        <li><a href="http://facebook.com/airpair"><img src="/img/fb.png" /></a></li>
+        <li><a href="https://plus.google.com/105075583437166504731‎"><img src="/img/gp.png" /></a></li>
+        <li><a href="http://www.linkedin.com/company/airpair"><img src="/img/in.png" /></a></li>
+      </ul>
+
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</div><!-- #secondary -->
 	<?php endif; ?>
