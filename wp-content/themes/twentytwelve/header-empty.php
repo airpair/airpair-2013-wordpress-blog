ff1e0852ffebeb6e5e0603ab865348e4864addc0
jkresner <jkresner@yahoo.com.au>
2013-09-28 10:49:28 -0700

    playing with wp_head

--- wp-content/themes/twentytwelve/header-empty.php
+++ wp-content/themes/twentytwelve/header-empty.php
@@ -20,8 +20,9 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
+<?php wp_head(); ?>
 
-<link rel="stylesheet" href="www.airpair.com/stylesheets/landing_jk.css">
+<link rel="stylesheet" href="http://www.airpair.com/stylesheets/landing_jk.css">
 <script src="//cdn.optimizely.com/js/280598780.js"></script>
 
 </head>
