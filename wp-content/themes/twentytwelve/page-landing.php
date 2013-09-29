59bc6f72c007057a4c90cee4f0f1b24ea4a0d0f9
jkresner <jkresner@yahoo.com.au>
2013-09-29 13:51:06 -0700

    Add pricing to page layout and try fix footer ui

--- wp-content/themes/twentytwelve/page-landing.php
+++ wp-content/themes/twentytwelve/page-landing.php
@@ -22,17 +22,45 @@ Template Name: Landing
 
 <?php the_content(); ?>
 
-<div style="clear:both;height:5px"></div>
-  <div class="footer" style="background:black;height:0px;padding:20px;color:white">
-    <div>
-      &copy; <a href="http://airpair.com/" style="color:white">airpair, inc.</a> 2013
+
+<div class='fold-border'></div>
+<div id="cost" class="outer-container cf">
+  <div class="middle-container">
+    <div class="inner-container marketing">
+      <div class="tagline"><h1>How much does it cost?</h1></div>
+
+      <p>Airpair is a marketplace. Experts set their own rate and we match you corresponding to your budget.</p>
+
+      <table>
+        <tr>
+          <th>$60<i>/hr</i></th>
+          <th>$90<i>/hr</i></th>
+          <th>$130<i>/hr</i></th>
+          <th>$200<i>/hr</i></th>
+          <th>$300<i>/hr</i></th>
+        </tr>
+        <tr>
+          <td><div>Access 15%</div> of our experts <em>Solve any beginner challenge</em></td>
+          <td><div>Access 40%</div> of our experts <em>Write software like an expert</em></td>
+          <td><div>Access 60%</div> of our experts <em>Solve deep technical problems</em></td>
+          <td><div>Access 90%</div> of our experts <em>Get solutions from world class talent</em></td>
+          <td><div>Access 100%</div> of our experts <em>Leverage industry thought leaders</em></td>
+        </tr>
+      </table>
+
+      <div class="inner-container">
+        <div style="clear:both"></div><br />
+        <a href="/find-an-expert" class="button" style="text-align:center;width:640px"> Get started</a>
+      </div>
     </div>
   </div>
 </div>
 
+<footer>
+    &copy; <a href="http://airpair.com/">airpair, inc. 2013 | </a>
+</footer>
+
 <script src="/javascripts/providers.js" defer onload="require('scripts/providers/all');"></script>
-<script src="http://i.po.st/static/v3/post-widget.js#publisherKey=miu9e01ukog3g0nk72m6" type="text/javascript"></script>
 
 </body>
-
 </html>
