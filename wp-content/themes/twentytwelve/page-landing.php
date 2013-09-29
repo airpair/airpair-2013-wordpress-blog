ff1d58bccd3c777ce48ae0a5a64f85eb0bdfeea4
jkresner <jkresner@yahoo.com.au>
2013-09-29 13:26:11 -0700

    Fixing Landing page rendering

--- wp-content/themes/twentytwelve/page-landing.php
+++ wp-content/themes/twentytwelve/page-landing.php
@@ -6,9 +6,21 @@ Template Name: Landing
 
 <?php get_header('landing'); ?>
 
+<nav class="cf">
+  <a class="logo" href="/"></a>
+  <ul class="social">
+    <li><a href="http://twitter.com/airpair" target="_blank" class="twitter"></a></li>
+    <li><a href="http://facebook.com/airpair" target="_blank" class="facebook"></a></li>
+  </ul>
+  <ul class="navigation">
+    <li><a href="/pair-programming"><span>{</span> pair programming <span>}</span></a></li>
+    <li class="active"><a href="/code-review"><span>{</span> code review <span>}</span></a></li>
+    <li><a href="/problem-solving"><span>{</span> problem solving <span>}</span></a></li>
+    <li><a href="/code-mentoring"><span>{</span> code mentoring <span>}</span></a></li>
+  </ul>
+</nav>
 
-<?php get_template_part( 'content', 'page' ); ?>
-
+<?php the_content(); ?>
 
 <div style="clear:both;height:5px"></div>
   <div class="footer" style="background:black;height:0px;padding:20px;color:white">
