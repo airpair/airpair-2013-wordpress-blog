7ffca1d0e2496b37dc33dff25c346b3bd0dcc355
jkresner <jkresner@yahoo.com.au>
2013-09-29 12:49:59 -0700

    Removing more junk from the head

--- wp-content/themes/twentytwelve/header-landing.php
+++ wp-content/themes/twentytwelve/header-landing.php
@@ -21,10 +21,15 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/css/landing.css">
-<script src="//cdn.optimizely.com/js/280598780.js"></script>
+<!-- <script src="//cdn.optimizely.com/js/280598780.js"></script> -->
 
-<?php remove_action( 'wp_enqueue_scripts', 'dd_wp_enqueue_styles' ); ?>
-<?php remove_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles' ); ?>
+<?php remove_action('wp_head' 'recent_comments_style'); ?>
+<?php remove_action('wp_head', 'admin-bar-css'); ?>
+<?php remove_action('wp_head', 'boxes-css'); ?>
+<?php remove_action('wp_head', 'wlwmanifest_link'); ?>
+<?php remove_action('wp_head', 'rsd_link'); ?>
+<?php remove_action('wp_enqueue_scripts', 'dd_wp_enqueue_styles'); ?>
+<?php remove_action('wp_enqueue_scripts', 'twentytwelve_scripts_styles'); ?>
 <?php wp_head(); ?>
 
 </head>
