e00d0cdfce5240b2ab1f34096f8633775af66462
jkresner <jkresner@yahoo.com.au>
2013-09-29 09:55:04 -0700

    Try with ids

--- wp-content/themes/twentytwelve/header-empty.php
+++ wp-content/themes/twentytwelve/header-empty.php
@@ -23,7 +23,8 @@
 <link rel="stylesheet" href="http://www.airpair.com/stylesheets/landing_jk.css">
 <script src="//cdn.optimizely.com/js/280598780.js"></script>
 
-<?php wp_dequeue_style( 'twentytwelve-fonts' ); ?>
+<?php wp_dequeue_style( 'twentytwelve-fonts-css' ); ?>
+<?php wp_dequeue_style( 'twentytwelve-style-css' ); ?>
 <?php wp_head(); ?>
 
 </head>
