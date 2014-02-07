4adb5b4093ae736d0ef7563f5a10221df1acf3b7
Igor Lebovic <igor.lebovic@gmail.com>
2014-02-07 00:04:13 -0800

    disallowing /l/ and adding the sem template

---
+++ wp-content/themes/epik/page_sem.php
@@ -0,0 +1,16 @@
+<?php
+ 
+//* Force full width content layout
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+ 
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+add_action( 'genesis_loop', 'custom_do_loop' );
+ 
+function custom_do_loop() { ?>
+ 
+	<p>My page content goes here</p>
+ 
+<?php }
+ 
+//* Run the Genesis loop
+genesis();
