43ec6dac35264292bfbc5577df898bdcc652a252
Maksim Ioffe <maksim.ioffe@gmail.com>
2014-02-09 14:37:39 -0800

    commenting out code per Igor

--- wp-content/themes/epik/page_sem.php
+++ wp-content/themes/epik/page_sem.php
@@ -5,14 +5,14 @@
 //* Force full width content layout
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
  
-remove_action( 'genesis_loop', 'genesis_do_loop' );
+/*remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'custom_do_loop' );
  
 function custom_do_loop() { ?>
  
 	<p>My page content goes here</p>
  
-<?php }
+<?php }*/
  
 //* Run the Genesis loop
 genesis();
