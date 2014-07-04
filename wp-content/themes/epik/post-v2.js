7b4c6b439c080f057e14360c09a3ff1d03636af3
Igor Lebovic <igor.lebovic@gmail.com>
2014-07-03 17:47:10 -0700

    tutorial css and js files added

---
+++ wp-content/themes/epik/post-v2.js
@@ -0,0 +1,36 @@
+jQuery(function($){
+    
+
+    // move  into header #image-featured
+    var vid = $(".entry-content #featured-video").detach()
+    $(".entry-header").prepend(vid)
+
+    // Fixed table of contents on scroll
+    setTimeout(function() {
+      var tableOfContents = $('#table-of-contents').offset().top;
+      $(window).scroll(function(e){
+        if (window.scrollY > tableOfContents) {
+          $('#table-of-contents').addClass('affix');
+        } else {
+          $('#table-of-contents').removeClass('affix');
+        }
+        console.log(window.scrollY);
+      });
+
+    }, 2000)
+
+    // Animated anchors
+    $("a[href^='#']").click(function(e){
+      e.preventDefault();
+      $("html,body").stop().animate({
+        scrollTop:$(this.hash).offset().top
+      },500);
+    });
+
+
+
+    console.log(vid);
+
+
+
+  });
