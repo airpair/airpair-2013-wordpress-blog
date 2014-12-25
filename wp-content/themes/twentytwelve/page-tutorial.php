7b1f89eab2a5ea4d796ddfb0ff3f349cde62fc98
Jonathon Kresner <jkrez@JK.local>
2014-12-25 10:30:42 -0800

    blah

---
+++ wp-content/themes/twentytwelve/page-tutorial.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Landing
+*/
+?>
+<div>
+  <link rel="stylesheet" type="text/css" href="/css/ap.css">
+
+  <nav>
+    <ul>
+      <li></li>
+    </ul>
+  <nav>
+
+  <?php the_content(); ?>
+
+  <script src="/javascripts/bookme.js"></script>
+  <footer style="color:white;"></footer>
+
+</div>
+
+<script type="text/javascript"> window.useOlark = true; </script>
+<script src="/javascripts/providers.js" defer onload="require('scripts/providers/all');"></script>
+
+</body>
+</html>
